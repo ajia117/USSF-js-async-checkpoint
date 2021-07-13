b590dd6d58ed57b2ed4fb43709953c3cbf6f352e
Evan Fillman <efillman@gmail.com>
2021-07-13 16:33:06 -0400

    improved main function

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const fsPromises = require('fs/promises');
 const fetch = require('node-fetch');
 
+let args = process.argv // arguments
+main(args[2])
 
-//let pokemonToCheck =
-readPokemonNamesFile('./pokemon_names.txt') //right now we have [ 'bulbasaur', 'charmander']
-  .then((pokemonArray) => {
-    Promise.all(pokemonArray.map((pokemon) => {
-      return fetchPokeAPI (pokemon);
-    }))
-      .then((pokemon) => { return printPokemon(pokemon) })
-      .then((finalPrint) => {console.log(finalPrint)});
-  });
+function main (fileName='./pokemon_names.txt') {
+  readPokemonNamesFile(fileName) //right now we have [ 'bulbasaur', 'charmander']
+    .then((pokemonArray) => { return fetchAllPokemon(pokemonArray)})
+    .then((pokemon) => { return printPokemon(pokemon)})
+    .then((finalPrint) => {console.log(finalPrint)});
+}
 
+/**
+ * fetch all pokemon
+ *
+ * @param {*} pokemonArray an array of pokemon names
+ * @returns an array of all fetches from the pokemon API
+ */
+function fetchAllPokemon (pokemonArray) {
+  return Promise.all(pokemonArray.map((pokemon) => {
+    return fetchPokeAPI (pokemon);
+  }));
+}
 
 /**
  * returns a promise with an array of pokemon names read from the input file
@@ -75,3 +85,17 @@ function printPokemon (pokemon) {
 //dynamically define input file
 //pre-check input names before making individual call
 // node wrapped API: https://github.com/PokeAPI/pokedex-promise-v2
+
+// 1) Add error handling for "bad arguments" using .catch().
+// * What happens if I pass in a non-existent file?
+// * What happens if I pass in NO arguments
+
+// 2) Log the output of the HTTP pokemon to a JSON file called "PokemonAttributes".
+//  * If the pokemon is already on your attribute list - then return the pokemon from the PokemonAttributes file instead of making a costly HTTP call.
+
+// 3) Accept an argument (T or F) that checks if the first two pokemon were to fight - which pokemon would have the advantage (based on types).
+//  * Check out damage_relations in the Pokemon.Type section
+//      https://pokeapi.co/docs/v2#types
+//  * If pokemon1 would have the advantage log out something like "Bulbasaur has the advantage over Goldeen".
+
+// 4) Some other fun functionality you and you partner agree to!
